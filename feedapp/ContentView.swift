d13232fb4966f090ac9472bb40075f26ffe11129
tusharshr <tushar@mysuperhumanrace.com>
2022-04-26 17:40:05 +0530

    Initial Commit

---
+++ feedapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  feedapp
+//
+//  Created by Gagandeep on 26/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ feedapp/feedappApp.swift
@@ -0,0 +1,17 @@
+//
+//  feedappApp.swift
+//  feedapp
+//
+//  Created by Gagandeep on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct feedappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
